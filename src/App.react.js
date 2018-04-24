7503456b0612c164af8a250a363caf40dea72505
AaronCoplan <afcoplan@gmail.com>
2018-04-24 01:09:59 +0000

    Reformatted w prettier.

--- src/App.react.js
+++ src/App.react.js
@@ -1,7 +1,7 @@
 // @flow
 
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Index from './Index.react';
 import Elements from './Elements.react';
@@ -9,16 +9,16 @@ import Generic from './Generic.react';
 
 type Props = {||};
 
-function App(props: Props): React.Node {  
-    return (
-      <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route exact={true} path="/" component={Index} />
-          <Route exact={true} path="/elements" component={Elements} />
-          <Route exact={true} path="/generic" component={Generic} />
-        </Switch>
-      </Router>
-    );
+function App(props: Props): React.Node {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact={true} path="/" component={Index} />
+        <Route exact={true} path="/elements" component={Elements} />
+        <Route exact={true} path="/generic" component={Generic} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/PageWrapper.react.js
+++ src/components/PageWrapper.react.js
@@ -5,22 +5,26 @@ import * as React from 'react';
 import Menu from './Menu.react';
 
 type Props = {|
-  +children: React.Node,
+  +children: React.Node
 |};
 
 function PageWrapper(props: Props): React.Node {
-  const {children} = props;
+  const { children } = props;
   return (
     <div id="wrapper">
       <header id="header">
         <div className="inner">
           <a href="/" className="logo">
-            <span className="symbol"><img src="images/logo.svg" alt="" /></span>
+            <span className="symbol">
+              <img src="images/logo.svg" alt="" />
+            </span>
             <span className="title">ACM Labs</span>
           </a>
           <nav>
             <ul>
-              <li><a href="#menu">Menu</a></li>
+              <li>
+                <a href="#menu">Menu</a>
+              </li>
             </ul>
           </nav>
         </div>
